359f5deefb7d791353fd18cca928a193e42da2eb
donggeon-choi <127081455+donggeon-choi@users.noreply.github.com>
2023-04-18 17:38:14 +0900

    Update 14-2.split.py

--- src/limo_opencv/scripts/14-2.split.py
+++ src/limo_opencv/scripts/14-2.split.py
@@ -24,7 +24,7 @@ class Split:
 
         # Get the file path of the "opencv_edu" package
         rospack = rospkg.RosPack()
-        self.file_path = rospack.get_path("opencv_edu")
+        self.file_path = rospack.get_path("limo_opencv")
         self.file_path += "/scripts/"
 
         # Set the file path for the image file
